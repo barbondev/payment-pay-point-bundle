c8fcf44e9e5533832e73863a7df1cfd8058b7d3d
Ashley <a.dawson@ashleydawson.co.uk>
2014-11-03 15:58:05 +0000

    Added test harness and bootstrap

---
+++ Tests/Plugin/PayPointHostedPluginTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Barbondev\Payment\PayPointHostedBundle\Tests\Plugin;
+
+/**
+ * Class PayPointHostedPluginTest
+ *
+ * @package Barbondev\Payment\PayPointHostedBundle\Tests\Plugin
+ * @author Ashley Dawson <ashley.dawson@barbon.com>
+ */
+class PayPointHostedPluginTest extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ Tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+$loader = require __DIR__ . '/../vendor/autoload.php';
+
+$loader->add('Barbondev\Payment\PayPointHostedBundle\Tests', __DIR__);
