98c9a17420ee5b82a8a1bac63188a28097b2e6ee
Ashley <a.dawson@ashleydawson.co.uk>
2014-11-06 16:33:18 +0000

    Added entity manager and logger to callback controller

--- Controller/CallbackController.php
+++ Controller/CallbackController.php
@@ -2,6 +2,11 @@
 
 namespace Barbondev\Payment\PayPointHostedBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Common\Persistence\ObjectManager;
+use Psr\Log\LoggerInterface;
+
 /**
  * Class CallbackController
  *
@@ -10,29 +15,57 @@ namespace Barbondev\Payment\PayPointHostedBundle\Controller;
  */
 class CallbackController
 {
-    public function gatewayCallbackAction()
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * Constructor
+     *
+     * @param ObjectManager $em
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(ObjectManager $em, LoggerInterface $logger)
     {
+        $this->em = $em;
+        $this->logger = $logger;
+    }
+
+    public function gatewayCallbackAction(Request $request)
+    {
+        $params = $request->query;
+
+        if ( ! $params->has('trans_id')) {
+            // todo: log and throw exception
+        }
+
         // ...
 
-/*
-$this->request->query->all()
-Array
-(
-    [valid] => true
-    [trans_id] => 56
-    [code] => A
-    [auth_code] => 9999
-    [expiry] => 0915
-    [card_no] => 1111
-    [customer] => MR T TEST
-    [amount] => 24.28
-    [ip] => 195.59.185.214
-    [test_status] => true
-    [mpi_status_code] => 210
-    [mpi_message] => Other Result
-    [hash] => d422b47f7cfcc74eb837b212beb5ec0e
-)
-*/
-
+        /*
+        $this->request->query->all()
+        Array
+        (
+            [valid] => true
+            [trans_id] => 56
+            [code] => A
+            [auth_code] => 9999
+            [expiry] => 0915
+            [card_no] => 1111
+            [customer] => MR T TEST
+            [amount] => 24.28
+            [ip] => 195.59.185.214
+            [test_status] => true
+            [mpi_status_code] => 210
+            [mpi_message] => Other Result
+            [hash] => d422b47f7cfcc74eb837b212beb5ec0e
+        )
+        */
+        return new Response('<pre>'.print_r($request->query->all(), true).'</pre>');
     }
 }
