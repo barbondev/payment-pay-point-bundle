13264ff0de327ee5948aa56bd5f7d87d64a9ce46
Ashley <a.dawson@ashleydawson.co.uk>
2014-11-06 10:28:23 +0000

    Implemented stub for handling payment deposit

--- Plugin/PayPointHostedPlugin.php
+++ Plugin/PayPointHostedPlugin.php
@@ -2,7 +2,10 @@
 
 namespace Barbondev\Payment\PayPointHostedBundle\Plugin;
 
+use JMS\Payment\CoreBundle\Model\FinancialTransactionInterface;
 use JMS\Payment\CoreBundle\Plugin\AbstractPlugin;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Class PayPointHostedPlugin
@@ -13,10 +16,42 @@ use JMS\Payment\CoreBundle\Plugin\AbstractPlugin;
 class PayPointHostedPlugin extends AbstractPlugin
 {
     /**
+     * @var EngineInterface
+     */
+    private $templating;
+
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
+
+    /**
+     * Constructor
+     *
+     * @param EventDispatcherInterface $eventDispatcher
+     * @param EngineInterface $templating
+     * @param bool $isDebug
+     */
+    public function __construct(EventDispatcherInterface $eventDispatcher, EngineInterface $templating, $isDebug = false)
+    {
+        parent::__construct($isDebug);
+        $this->templating = $templating;
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function processes($paymentSystemName)
     {
-        return ('paypoint_hosted' == $paymentSystemName);
+        return ('paypoint_hosted' === $paymentSystemName);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function approveAndDeposit(FinancialTransactionInterface $transaction, $retry)
+    {
+        // ...
     }
 }
